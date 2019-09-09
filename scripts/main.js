95f270d998e62b6b1e3012b1f57a499e092fa672
Qw8415 <g.szul00@gmail.com>
2019-09-09 15:27:54 +0200

    Scripted score board and result statement

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rules = {
+const result_p = document.querySelector(".result > p");
+const winnerElements = {
     rock: ['scissors', 'lizard'],
     paper: ['rock', 'spock'],
     scissors: ['paper', 'lizard'],
@@ -17,15 +17,31 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 5)];
 }
 
+function draw() {
+    result_p.innerHTML = "draw!";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_p.innerHTML = `${userChoice} beats ${computerChoice}. You win!`;
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${computerChoice} beats ${userChoice}. You lose!`;
+}
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     console.log(userChoice, computerChoice);
     if (computerChoice === userChoice) {
-        console.log("draw!");
+        draw();
     } else {
-        rules[userChoice].includes(computerChoice) ? console.log("User win!") : console.log("Computer win!");
+        winnerElements[userChoice].includes(computerChoice) ?
+            win(userChoice, computerChoice) : lose(userChoice, computerChoice);
     }
-
 }
 
 function main() {
